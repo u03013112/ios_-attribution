58f61e1d1ba1738880d72c6e9771838c36be74e5
u03013112 <u03013112@hotmail.com>
2022-10-11 17:46:54 +0800

    main

--- src/predict.py
+++ src/predict.py
@@ -17,3 +17,6 @@ def main(sinceTimeStr,unitlTimeStr):
     print('skan付费总金额：',skanUsdSum)
 
     print('总金额差（skan付费总金额 + 预测自然量付费总金额 + 预测null总付费金额） / AF付费总金额：',(skanUsdSum + predictOrganicUsdSum + predictNullUsdSum)/afUsdSum)
+
+if __name__ == "__main__":
+    main('20220601','20220630')
