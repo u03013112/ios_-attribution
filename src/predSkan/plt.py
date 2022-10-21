751a23e6757faef38612365fdf0517fa35e01549
u03013112 <u03013112@hotmail.com>
2022-10-21 16:09:56 +0800

    添加分段

--- src/predSkan/plt.py
+++ src/predSkan/plt.py
@@ -49,7 +49,25 @@ def totalCvR7Less50():
         plt.savefig('/src/data/totalCvR7L50_%d.png'%(cv))
         print('save to /src/data/totalCvR7L50_%d.png'%(cv))
 
+# 过滤掉平均7日付费过高的数据，最高每人400usd
+def totalCvR7F1():
+    print('画图，大盘数据，每个cv一张图，count是x，7日总收入是y')
+    df = pd.read_csv(getFilename('totalData%s_%s'%('20220501','20220930')))
+    df.loc[df.sumr7usd > df['count'] * 400,'sumr7usd']=df['count']*400
+    for cv in range(64):
+        dfCv = df.loc[df.cv == cv]
+        count = dfCv['count'].to_numpy()
+        r7 = dfCv['sumr7usd'].to_numpy()
+        plt.title("cv = %d"%cv) 
+        plt.xlabel("count") 
+        plt.ylabel("r7usd sum") 
+        plt.plot(count,r7,'ro')
+        plt.savefig('/src/data/totalCvR7F1_%d.png'%(cv))
+        print('save to /src/data/totalCvR7F1_%d.png'%(cv))
+
+
 if __name__ == '__main__':
     # totalCvR7()
     # totalCvR7Less1000()
-    totalCvR7Less50()
+    # totalCvR7Less50()
+    totalCvR7F1()
--- src/predSkan/totalAI1.py
+++ src/predSkan/totalAI1.py
@@ -1,18 +1,18 @@
-# 大盘预测AI
-# 直接用大盘数据来做拟合
-
-# 流程确定后，尝试更改分组，查看效果
+# 大盘预测AI，分段预测
+# 其中发现count小于20的数据偏差比较大，先用这个档位分一下
+# 还有将过高的大R过滤一下
 import pandas as pd
 
 import sys
 sys.path.append('/src')
 from src.predSkan.data import getTotalData
 from src.tools import getFilename
-from src.googleSheet import GSheet
-# 暂定方案是先将数据分组，比如直接分为64组
-groupList = [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23],[24],[25],[26],[27],[28],[29],[30],[31],[32],[33],[34],[35],[36],[37],[38],[39],[40],[41],[42],[43],[44],[45],[46],[47],[48],[49],[50],[51],[52],[53],[54],[55],[56],[57],[58],[59],[60],[61],[62],[63]]
-# groupList = [[0],[1,2,3,4,5,6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20],[21,22,23,24,25,26,27,28,29,30],[31,32,33,34,35,36,37,38,39,40],[41,42,43,44,45,46,47,48,49,50],[51,52,53,54,55,56,57,58,59,60],[61,62],[63]]
 
+# 暂定方案是先将数据分组，比如直接分为64组
+# 分段的部分0不分，不付费的用户数量总是很多，不会出现人数较少的情况
+groupList = [[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23],[24],[25],[26],[27],[28],[29],[30],[31],[32],[33],[34],[35],[36],[37],[38],[39],[40],[41],[42],[43],[44],[45],[46],[47],[48],[49],[50],[51],[52],[53],[54],[55],[56],[57],[58],[59],[60],[61],[62],[63]]
+# 分段，暂时只分一段，0~20一段，20~正无穷一段
+segList = [0,20,10e9]
 import datetime
 # 各种命名都用这个后缀，防止重名
 filenameSuffix = datetime.datetime.now().strftime('_%Y%m%d_%H')
@@ -24,6 +24,8 @@ def dataStep0(sinceTimeStr,unitlTimeStr):
 # 从本地文件取数据，跑过步骤0的可以直接从这里开始，更快速
 def dataStep1(sinceTimeStr,unitlTimeStr):
     df = pd.read_csv(getFilename('totalData%s_%s'%(sinceTimeStr,unitlTimeStr)))
+    # 把超过400美金的过滤一下，400这个值是随意定的，可以改
+    df.loc[df.sumr7usd > df['count'] * 400,'sumr7usd']=df['count']*400
     return df
 
 # 对每组数据分别整理
@@ -40,12 +42,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 # 单独训练一定次数后，保存结果
-def createMod(createModFun):
-    modList = []
-    for i in range(len(groupList)):
-        mod = createModFun()
-        modList.append(mod)
-    return modList
 
 def createModFunc1():
     mod = keras.Sequential(
@@ -113,8 +109,24 @@ def createModFunc6():
     mod.compile(optimizer='adadelta',loss='mape')
     return mod
 
+def createMod(createModFun):
+    modList = []
+    for i in range(len(groupList)):
+        modList2 = []
+        for j in range(len(segList)-1):
+            mod = createModFun()
+            modList2.append(mod)
+        modList.append(modList2)
+    return modList
+
 createModList = [
     {
+        'name':'mod1',
+        'createModFunc':createModFunc1
+    },{
+        'name':'mod2',
+        'createModFunc':createModFunc2
+    },{
         'name':'mod3',
         'createModFunc':createModFunc3
     },{
@@ -126,47 +138,53 @@ createModList = [
     },{
         'name':'mod6',
         'createModFunc':createModFunc6
-    },{
-        'name':'mod1',
-        'createModFunc':createModFunc1
-    },{
-        'name':'mod2',
-        'createModFunc':createModFunc2
     }
 ]
 def train(dataDf2,modList,modName):
     callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
     for i in range(len(groupList)):
-        trainDf = dataDf2.loc[(dataDf2.group == i) & (dataDf2.install_date < '2022-09-01')].groupby('install_date').agg('sum')
-        testDf = dataDf2.loc[(dataDf2.group == i) & (dataDf2.install_date >= '2022-09-01')].groupby('install_date').agg('sum')
-        trainX = trainDf['count'].to_numpy()
-        trainY = trainDf['sumr7usd'].to_numpy()
-        testX = testDf['count'].to_numpy()
-        testY = testDf['sumr7usd'].to_numpy()
-        mod = modList[i]
-        history = mod.fit(trainX, trainY, epochs=500000, validation_data=(testX,testY)
-        ,callbacks=[callback]
-        # ,verbose=0
-        )
-        historyDf = pd.DataFrame(data=history.history)
-        historyDf.to_csv(getFilename('history%d_%s%s'%(i,modName,filenameSuffix)))
-        modFileName = '/src/src/predSkan/mod/mTotal%d_%s%s.h5'%(i,modName,filenameSuffix)
-        mod.save(modFileName)
-        print('save %s,loss:%f'%(modFileName,history.history['loss'][-1]))
-        logFilename = '/src/src/predSkan/log/log%d.log'%(i)
-        # 记录日志文件
-        with open(logFilename, 'a+') as f:
-            if 'val_loss' in history.history:
-                f.write('%s %f %f\n'%(modFileName,history.history['loss'][-1],history.history['val_loss'][-1]))
-            else:
-                f.write('%s %f -\n'%(modFileName,history.history['loss'][-1]))
+        for j in range(len(segList)-1):
+            countMin = segList[j]
+            countMax = segList[j+1]
+
+            trainDf = dataDf2.loc[(dataDf2.group == i) & (dataDf2.install_date < '2022-09-01')].groupby('install_date').agg('sum')
+            testDf = dataDf2.loc[(dataDf2.group == i) & (dataDf2.install_date >= '2022-09-01')].groupby('install_date').agg('sum')
+
+            trainDf = trainDf.loc[(trainDf['count']>countMin)&(trainDf['count']<=countMax)]
+            testDf = testDf.loc[(testDf['count']>countMin)&(testDf['count']<=countMax)]
+
+            trainX = trainDf['count'].to_numpy()
+            trainY = trainDf['sumr7usd'].to_numpy()
+            testX = testDf['count'].to_numpy()
+            testY = testDf['sumr7usd'].to_numpy()
+            mod = modList[i]
+            history = mod.fit(trainX, trainY, epochs=50000 , validation_data=(testX,testY)
+            ,callbacks=[callback]
+            # ,verbose=0
+            )
+            historyDf = pd.DataFrame(data=history.history)
+            historyDf.to_csv(getFilename('historySeg%d_%d_%s%s'%(i,j,modName,filenameSuffix)))
+            modFileName = '/src/src/predSkan/mod/mTotalSeg%d_%d_%s%s.h5'%(i,j,modName,filenameSuffix)
+            mod.save(modFileName)
+            print('save %s,loss:%f'%(modFileName,history.history['loss'][-1]))
+            logFilename = '/src/src/predSkan/log/logSeg%d_%d.log'%(i,j)
+            # 记录日志文件
+            with open(logFilename, 'a+') as f:
+                with open(logFilename, 'a+') as f:
+                    if 'val_loss' in history.history:
+                        f.write('%s %f %f\n'%(modFileName,history.history['loss'][-1],history.history['val_loss'][-1]))
+                    else:
+                        f.write('%s %f -\n'%(modFileName,history.history['loss'][-1]))
 
 def loadMod(modName,suffix):
     modList = []
     for i in range(len(groupList)):
-        modFileName = '/src/src/predSkan/mod/mTotal%d_%s_%s.h5'%(i,modName,suffix)
-        mod = tf.keras.models.load_model(modFileName)
-        modList.append(mod)
+        modList2 = []
+        for j in range(len(segList)-1):
+            modFileName = '/src/src/predSkan/mod/mTotalSeg%d_%d_%s%s.h5'%(i,j,modName,suffix)
+            mod = tf.keras.models.load_model(modFileName)
+            modList2.append(mod)
+        modList.append(modList2)
     return modList
 
 import numpy as np
