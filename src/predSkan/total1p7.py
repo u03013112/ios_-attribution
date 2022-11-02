f3aa1f84a7bab28357a0df67cd01712343629454
u03013112 <u03013112@hotmail.com>
2022-11-02 15:18:29 +0800

    切换到线上状态

--- src/predSkan/total1p7.py
+++ src/predSkan/total1p7.py
@@ -8,12 +8,20 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
-dayStr = '20220902'
-modName = '/src/src/predSkan/mod/mod02582-23.44.h5'
-import sys
-sys.path.append('/src')
-from src.maxCompute import execSql
-from src.tools import getFilename
+# dayStr = '20220902'
+# modName = '/src/src/predSkan/mod/mod02582-23.44.h5'
+# import sys
+# sys.path.append('/src')
+# from src.maxCompute import execSql
+# from src.tools import getFilename
+
+
+dayStr = args['dayStr']
+modName = 'mod02582-23.44.h5'
+def execSql(sql):
+    with o.execute_sql(sql).open_reader() as reader:
+        pd_df = reader.to_pandas()
+        return pd_df
 
 sql = '''
     select 
@@ -24,12 +32,6 @@ sql = '''
 ;
 '''
 afCvMapDataFrame = execSql(sql)
-# dayStr = args['dayStr']
-# modName = 'mod02582-23.44.h5'
-# def execSql(sql):
-#     with o.execute_sql(sql).open_reader() as reader:
-#         pd_df = reader.to_pandas()
-#         return pd_df
 
 groupList = [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23],[24],[25],[26],[27],[28],[29],[30],[31],[32],[33],[34],[35],[36],[37],[38],[39],[40],[41],[42],[43],[44],[45],[46],[47],[48],[49],[50],[51],[52],[53],[54],[55],[56],[57],[58],[59],[60],[61],[62],[63]]
 
@@ -212,7 +214,32 @@ def predict(dayStr):
 
     return yp
 
-if __name__ == '__main__':
-    print(predict(dayStr))
+from odps.models import Schema, Column, Partition
+def createTable():
+    columns = [
+        Column(name='p7usd', type='double', comment='predict d7 revenue in usd')
+    ]
+    partitions = [
+        Partition(name='install_date', type='string', comment='like 20221018')
+    ]
+    schema = Schema(columns=columns, partitions=partitions)
+    table = o.create_table('topwar_iosglobal_total1p7', schema, if_not_exists=True)
+    return table
 
-    
+# import pyarrow as pa
+def writeTable(df,dayStr):
+    t = o.get_table('topwar_iosglobal_total1p7')
+    t.delete_partition('install_date=%s'%(dayStr), if_exists=True)
+    with t.open_writer(partition='install_date=%s'%(dayStr), create_partition=True, arrow=True) as writer:
+        # batch = pa.RecordBatch.from_pandas(df)
+        # writer.write(batch)
+        print(df)
+        writer.write(df)
+
+
+createTable()
+py = predict(dayStr)[0][0]
+writeTable(pd.DataFrame(data={
+    'install_date':dayStr,
+    'p7usd':py
+}),dayStr)
