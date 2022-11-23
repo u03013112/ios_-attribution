8cca991294e5bf679a1c3713639232478452bf50
u03013112 <u03013112@hotmail.com>
2022-11-23 14:31:50 +0800

    添加max2

--- src/predSkan/data.py
+++ src/predSkan/data.py
@@ -149,6 +149,157 @@ def getTotalData(sinceTimeStr,unitlTimeStr):
     return pd_df
 
 
+def getTotalDataMax(sinceTimeStr,unitlTimeStr,max = 200.0):
+    sinceTimeStr2 = list(sinceTimeStr)
+    sinceTimeStr2.insert(6,'-')
+    sinceTimeStr2.insert(4,'-')
+    sinceTimeStr2 = ''.join(sinceTimeStr2)
+
+    unitlTimeStr2 = list(unitlTimeStr)
+    unitlTimeStr2.insert(6,'-')
+    unitlTimeStr2.insert(4,'-')
+    unitlTimeStr2 = ''.join(unitlTimeStr2) + ' 23:59:59'
+    # 为了获得完整的7日回收，需要往后延长7天
+    unitlTime = datetime.datetime.strptime(unitlTimeStr,'%Y%m%d')
+    unitlTimeStr = (unitlTime+datetime.timedelta(days=7)).strftime('%Y%m%d')
+
+    whenStr = ''
+    for i in range(len(afCvMapDataFrame)):
+        min_event_revenue = afCvMapDataFrame.min_event_revenue[i]
+        max_event_revenue = afCvMapDataFrame.max_event_revenue[i]
+        if pd.isna(min_event_revenue) or pd.isna(max_event_revenue):
+            continue
+        whenStr += 'when r1usd>%d and r1usd<=%d then %d\n'%(min_event_revenue, max_event_revenue,i)
+
+    sql='''
+        select
+            cv,
+            count(*) as count,
+            sum(
+                case when r1usd_max is null then 0
+                    else r1usd_max
+                end) as sumR1usd,
+            sum(
+                case when r7usd_max is null then 0
+                    else r7usd_max
+                end) as sumR7usd,
+            install_date
+        from
+            (
+                select
+                    customer_user_id,
+                    case
+                        when r1usd = 0
+                        or r1usd is null then 0 
+                        % s
+                        else 63
+                    end as cv,
+                    case
+                        when r1usd >= %f
+                        then %f
+                        else r1usd
+                    end as r1usd_max,
+                    case
+                        when r7usd >= %f
+                        then %f
+                        else r7usd
+                    end as r7usd_max,
+                    install_date
+                from
+                    (
+                        SELECT
+                            t0.customer_user_id,
+                            t0.install_date,
+                            t1.r1usd,
+                            t1.r7usd
+                        FROM
+                            (
+                                select
+                                    customer_user_id,
+                                    install_date
+                                from
+                                    (
+                                        select
+                                            customer_user_id,
+                                            to_char(
+                                                to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                                "yyyy-mm-dd"
+                                            ) as install_date
+                                        from
+                                            ods_platform_appsflyer_events
+                                        where
+                                            app_id = 'id1479198816'
+                                            and event_name = 'install'
+                                            and zone = 0
+                                            and day >= %s
+                                            and day <= %s
+                                            and install_time >= "%s"
+                                            and install_time <= "%s"
+                                        union
+                                        all
+                                        select
+                                            customer_user_id,
+                                            to_char(
+                                                to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                                "yyyy-mm-dd"
+                                            ) as install_date
+                                        from
+                                            tmp_ods_platform_appsflyer_origin_install_data
+                                        where
+                                            app_id = 'id1479198816'
+                                            and zone = '0'
+                                            and install_time >= "%s"
+                                            and install_time <= "%s"
+                                    )
+                                group by
+                                    customer_user_id,
+                                    install_date
+                            ) as t0
+                            LEFT JOIN (
+                                select
+                                    customer_user_id,
+                                    to_char(
+                                        to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                        "yyyy-mm-dd"
+                                    ) as install_date,
+                                    sum(
+                                        case
+                                            when event_timestamp - install_timestamp <= 1 * 24 * 3600 then cast (event_revenue_usd as double)
+                                            else 0
+                                        end
+                                    ) as r1usd,
+                                    sum(
+                                        case
+                                            when event_timestamp - install_timestamp <= 7 * 24 * 3600 then cast (event_revenue_usd as double)
+                                            else 0
+                                        end
+                                    ) as r7usd
+                                from
+                                    ods_platform_appsflyer_events
+                                where
+                                    app_id = 'id1479198816'
+                                    and event_name = 'af_purchase'
+                                    and zone = 0
+                                    and day >= %s
+                                    and day <= %s
+                                    and install_time >= "%s"
+                                    and install_time <= "%s"
+                                group by
+                                    install_date,
+                                    customer_user_id
+                            ) as t1 ON t0.customer_user_id = t1.customer_user_id
+                    )
+            )
+        group by
+            cv,
+            install_date
+        ;
+    '''%(whenStr,max,max,max,max,sinceTimeStr,unitlTimeStr,sinceTimeStr2,unitlTimeStr2,sinceTimeStr2,unitlTimeStr2,sinceTimeStr,unitlTimeStr,sinceTimeStr2,unitlTimeStr2)
+    print(sql)
+    # return
+    pd_df = execSql(sql)
+    return pd_df
+
 # 获取前N天的收入并映射成cv
 def getTotalDataNDay(sinceTimeStr,unitlTimeStr,n):
     sinceTimeStr2 = list(sinceTimeStr)
@@ -586,6 +737,163 @@ def getTotalDataGroupByGeoMax(sinceTimeStr,unitlTimeStr,max=500.0):
     # return
     pd_df = execSql(sql)
     return pd_df
+
+# 首日收入也要抹平
+def getTotalDataGroupByGeoMax2(sinceTimeStr,unitlTimeStr,max=500.0):
+    sinceTime = datetime.datetime.strptime(sinceTimeStr,'%Y%m%d')
+    unitlTime = datetime.datetime.strptime(unitlTimeStr,'%Y%m%d')
+    
+    sinceTimeStr2 = sinceTime.strftime("%Y-%m-%d")
+    unitlTimeStr2 = unitlTime.strftime("%Y-%m-%d") + ' 23:59:59'
+    # 为了获得完整的7日回收，需要往后延长7天
+    unitlTime = datetime.datetime.strptime(unitlTimeStr,'%Y%m%d')
+    unitlTimeStr = (unitlTime+datetime.timedelta(days=7)).strftime('%Y%m%d')
+
+    whenStr = ''
+    for i in range(len(afCvMapDataFrame)):
+        min_event_revenue = afCvMapDataFrame.min_event_revenue[i]
+        max_event_revenue = afCvMapDataFrame.max_event_revenue[i]
+        if pd.isna(min_event_revenue) or pd.isna(max_event_revenue):
+            continue
+        whenStr += 'when r1usd>%d and r1usd<=%d then %d\n'%(min_event_revenue, max_event_revenue,i)
+
+    sql='''
+        select
+            cv,
+            count(*) as count,
+            sum(
+                case when r1usd_max is null then 0
+                    else r1usd_max
+                end) as sumR1usd,
+            sum(
+                case when r7usd_max is null then 0
+                    else r7usd_max
+                end) as sumR7usd,
+            install_date,
+            country_code
+        from
+            (
+                select
+                    customer_user_id,
+                    case
+                        when r1usd = 0
+                        or r1usd is null then 0 
+                        % s
+                        else 63
+                    end as cv,
+                    case
+                        when r1usd >= %f
+                        then %f
+                        else r1usd
+                    end as r1usd_max,
+                    case
+                        when r7usd >= %f
+                        then %f
+                        else r7usd
+                    end as r7usd_max,
+                    install_date,
+                    country_code
+                from
+                    (
+                        SELECT
+                            t0.customer_user_id,
+                            t0.install_date,
+                            t1.r1usd,
+                            t1.r7usd,
+                            t0.country_code
+                        FROM
+                            (
+                                select
+                                    customer_user_id,
+                                    install_date,
+                                    country_code
+                                from
+                                    (
+                                        select
+                                            customer_user_id,
+                                            to_char(
+                                                to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                                "yyyy-mm-dd"
+                                            ) as install_date,
+                                            country_code
+                                        from
+                                            ods_platform_appsflyer_events
+                                        where
+                                            app_id = 'id1479198816'
+                                            and event_name = 'install'
+                                            and zone = 0
+                                            and day >= %s
+                                            and day <= %s
+                                            and install_time >= "%s"
+                                            and install_time <= "%s"
+                                        union
+                                        all
+                                        select
+                                            customer_user_id,
+                                            to_char(
+                                                to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                                "yyyy-mm-dd"
+                                            ) as install_date,
+                                            country_code
+                                        from
+                                            tmp_ods_platform_appsflyer_origin_install_data
+                                        where
+                                            app_id = 'id1479198816'
+                                            and zone = '0'
+                                            and install_time >= "%s"
+                                            and install_time <= "%s"
+                                    )
+                                group by
+                                    customer_user_id,
+                                    install_date,
+                                    country_code
+                            ) as t0
+                            LEFT JOIN (
+                                select
+                                    customer_user_id,
+                                    to_char(
+                                        to_date(install_time, "yyyy-mm-dd hh:mi:ss"),
+                                        "yyyy-mm-dd"
+                                    ) as install_date,
+                                    sum(
+                                        case
+                                            when event_timestamp - install_timestamp <= 1 * 24 * 3600 then cast (event_revenue_usd as double)
+                                            else 0
+                                        end
+                                    ) as r1usd,
+                                    sum(
+                                        case
+                                            when event_timestamp - install_timestamp <= 7 * 24 * 3600 then cast (event_revenue_usd as double)
+                                            else 0
+                                        end
+                                    ) as r7usd
+                                from
+                                    ods_platform_appsflyer_events
+                                where
+                                    app_id = 'id1479198816'
+                                    and event_name = 'af_purchase'
+                                    and zone = 0
+                                    and day >= %s
+                                    and day <= %s
+                                    and install_time >= "%s"
+                                    and install_time <= "%s"
+                                group by
+                                    install_date,
+                                    customer_user_id
+                            ) as t1 ON t0.customer_user_id = t1.customer_user_id
+                    )
+            )
+        group by
+            cv,
+            install_date,
+            country_code
+        ;
+    '''%(whenStr,max,max,max,max,sinceTimeStr,unitlTimeStr,sinceTimeStr2,unitlTimeStr2,sinceTimeStr2,unitlTimeStr2,sinceTimeStr,unitlTimeStr,sinceTimeStr2,unitlTimeStr2)
+    print(sql)
+    # return
+    pd_df = execSql(sql)
+    return pd_df
+
 
 def getCost(sinceTimeStr,unitlTimeStr):
     sql='''
